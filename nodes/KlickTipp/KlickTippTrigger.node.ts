07c9770337a6cbaedbca8a7c92c8763e9f54a07e
Olex2324 <kovalenko.odev@gmail.com>
2025-03-12 15:40:45 +0200

    feat(trigger): add klicktipp event trigger

---
+++ nodes/KlickTipp/KlickTippTrigger.node.ts
@@ -0,0 +1,54 @@
+import type {
+	IDataObject,
+	IWebhookFunctions,
+	INodeType,
+	INodeTypeDescription,
+	IWebhookResponseData,
+} from 'n8n-workflow';
+import { NodeConnectionType } from 'n8n-workflow';
+
+export class KlickTippTrigger implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'KlickTipp Trigger',
+		name: 'klicktippTrigger',
+		icon: { light: 'file:klicktipp.svg', dark: 'file:klicktipp.dark.svg' },
+		group: ['trigger'],
+		version: 1,
+		description: 'Handle KlickTipp events via webhooks',
+		defaults: {
+			name: 'KlickTipp Trigger',
+		},
+		inputs: [],
+		outputs: [NodeConnectionType.Main],
+		credentials: [
+			{
+				name: 'klickTippApi',
+				required: true,
+			},
+		],
+		webhooks: [
+			{
+				name: 'default',
+				httpMethod: 'POST',
+				responseMode: 'onReceived',
+				path: 'webhook',
+			}
+		],
+		properties: [
+			{
+				displayName: 'For more information on creating a webhook in KlickTipp, see the online <a href="https://www.klicktipp.com/de/support/wissensdatenbank/outbound-erstellen-und-nutzen/" target="_blank">Help</a>.',
+				name: 'webhookSetup',
+				type: 'notice',
+				default: '',
+			},
+		],
+	};
+
+	// Handles the POST webhook request from KlickTipp.
+	async webhook(this: IWebhookFunctions): Promise<IWebhookResponseData> {
+		const req = this.getRequestObject();
+		return {
+			workflowData: [this.helpers.returnJsonArray(req.body as IDataObject)],
+		};
+	}
+}
