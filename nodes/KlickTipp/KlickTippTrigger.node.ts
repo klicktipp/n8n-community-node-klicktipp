f8e1c2aeded49998504f07b37ff0c4ddf9a80453
Olex2324 <kovalenko.odev@gmail.com>
2025-11-06 13:33:10 +0200

    feat(trigger): add path

--- nodes/KlickTipp/KlickTippTrigger.node.ts
+++ nodes/KlickTipp/KlickTippTrigger.node.ts
@@ -34,10 +34,21 @@ export class KlickTippTrigger implements INodeType {
 				name: 'default',
 				httpMethod: 'POST',
 				responseMode: 'onReceived',
-				path: 'webhook',
+				path: '={{ $parameter["path"] }}',
+				isFullPath: true,
 			},
 		],
+
 		properties: [
+			{
+				displayName: 'Path',
+				name: 'path',
+				type: 'string',
+				default: '',
+				placeholder: 'webhook',
+				description:
+					"The path to listen to, dynamic values could be specified by using ':', e.g. 'your-path/:dynamic-value'. Leave empty to use a unique, auto-generated path.",
+			},
 			{
 				displayName:
 					'For more information on creating a webhook in KlickTipp, see the online <a href="https://www.klicktipp.com/de/support/wissensdatenbank/outbound-erstellen-und-nutzen/" target="_blank">Help</a>.',
