3b25705f95469a84ea689cb6112a174bd2f3ed99
Olex2324 <kovalenko.odev@gmail.com>
2025-11-06 12:54:10 +0200

    fix(trigger): reverte changes

--- nodes/KlickTipp/KlickTippTrigger.node.ts
+++ nodes/KlickTipp/KlickTippTrigger.node.ts
@@ -34,21 +34,10 @@ export class KlickTippTrigger implements INodeType {
 				name: 'default',
 				httpMethod: 'POST',
 				responseMode: 'onReceived',
-    		path: '={{ $parameter["path"] }}',
-				isFullPath: true,
+				path: 'webhook',
 			},
 		],
-
 		properties: [
-			{
-				displayName: 'Path',
-				name: 'path',
-				type: 'string',
-				default: '',
-				placeholder: 'webhook',
-				description:
-					"The path to listen to, dynamic values could be specified by using ':', e.g. 'your-path/:dynamic-value'. Leave empty to use a unique, auto-generated path.",
-			},
 			{
 				displayName:
 					'For more information on creating a webhook in KlickTipp, see the online <a href="https://www.klicktipp.com/de/support/wissensdatenbank/outbound-erstellen-und-nutzen/" target="_blank">Help</a>.',
