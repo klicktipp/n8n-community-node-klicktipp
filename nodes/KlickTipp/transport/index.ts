69cbacc36dea600f51be8c556abd2d253eed8d0e
Olex2324 <kovalenko.odev@gmail.com>
2025-11-25 12:54:22 +0200

    fix(transport): change request options to http request options

--- nodes/KlickTipp/transport/index.ts
+++ nodes/KlickTipp/transport/index.ts
@@ -4,7 +4,7 @@ import {
 	IHttpRequestMethods,
 	ILoadOptionsFunctions,
 	IPollFunctions,
-	IRequestOptions,
+	IHttpRequestOptions,
 	NodeOperationError,
 } from 'n8n-workflow';
 
@@ -22,11 +22,10 @@ async function logout(
 		return;
 	}
 
-	const logoutOptions: IRequestOptions = {
+	const logoutOptions: IHttpRequestOptions = {
 		method: 'POST',
-		uri: `${BASE_URL}/account/logout`,
-		json: true,
-		rejectUnauthorized: verifySSL,
+		url: `${BASE_URL}/account/logout`,
+		skipSslCertificateValidation: !verifySSL,
 	};
 
 	try {
@@ -65,14 +64,12 @@ export async function apiRequest(
 		}),
 	};
 
-	const requestOptions: IRequestOptions = {
+	const requestOptions: IHttpRequestOptions = {
 		headers,
 		method,
 		qs: query,
-		uri: uri || `${BASE_URL}/${endpoint}`,
-		useQuerystring: false,
-		json: true,
-		rejectUnauthorized: verifySSL,
+		url: uri || `${BASE_URL}/${endpoint}`,
+		skipSslCertificateValidation: !verifySSL,
 		...option,
 	};
 
